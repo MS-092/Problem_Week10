80647792ea66c15ba4be21cace9d7ebeb7649684
Matthew S <mattstans092@gmail.com>
2023-12-02 23:58:47 +0700

    Update problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -11,4 +11,26 @@ def find_smallest_beautiful_number(x):
     return smallest_beautiful_number if smallest_beautiful_number != -1 else "No beautiful number divisible by {} so I will return 111111 as an exception".format(x)
 
 smallest = int(input("Enter number: "))
-print(find_smallest_beautiful_number(smallest))
+print(find_smallest_beautiful_number(smallest
+
+
+# Another solution using while loops, but it's so slow
+n = int(input("Enter a number: "))
+
+t = 0
+bn = n
+
+while t == 0:
+    if bn % n == 0:
+        nl = []
+        for d in str(bn):
+            if int(d) in nl:
+                pass
+            else:
+                nl.append(int(d))
+
+        if len(nl) == 1:
+            t += 1
+            print(f"The smallest beautiful number is {bn}")
+
+    bn += 1
